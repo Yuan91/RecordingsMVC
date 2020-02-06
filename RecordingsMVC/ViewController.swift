0723033fe3a814ab1dea9a2cca49885c2de48cf7
Yuan <smartdy@yeah.net>
2020-02-06 11:45:55 +0800

    Initial Commit

---
+++ RecordingsMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecordingsMVC
+//
+//  Created by du on 2020/2/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
